08255b7fe581335bd335b5609acf0c0e880ae470
Andrey Rakov <coma78rav@gmail.com>
2024-02-11 11:09:44 +0300

    задача 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Сгенерируйте массив из n чисел. Число n вводит пользователь
+
+// void InputArray(double[] array){
+//     for (int i = 0; i < array.Length; i++){
+//         array[i] = Math.Round(20 * new Random().NextDouble() - 10, 2); //(-10; 10)
+//         // NextDouble() - генерирует число в диапвзоне (0; 1)
+//         // Math.Round (число, кол-во знаков после запятой)
+//         // x (0; 10)
+//         // 20 * x (0; 20)
+//         // 20 * x - 10 (-10; 10)
+//     }
+// }
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// double [] array = new double [n]; // [0.0, 0.0, ..., 0.0]
+// InputArray (array);
+// System.Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+
+// 
